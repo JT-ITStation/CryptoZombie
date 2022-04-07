6d9ac7048453a402ce4e27ac15b6c30daef0b25d
it4station@gmail.com <it4station@gmail.com>
2022-04-08 00:42:16 +0200

    lesson 4

--- contracts/zombiehelper.sol
+++ contracts/zombiehelper.sol
@@ -1,27 +1,55 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 import "./zombiefeeding.sol";
+
 contract ZombieHelper is ZombieFeeding {
+    
+    uint levelUpFee = 0.001 ether;
 
-  // Commencez ici
-  modifier aboveLevel(uint _level, uint _zombieId){
-      require(zombies[_zombieId].level >= _level);
-      _;
-  }
+    modifier aboveLevel(uint256 _level, uint256 _zombieId) {
+        require(zombies[_zombieId].level >= _level);
+        _;
+    }
 
-  function changeName(uint _zombieId, string memory _newName) external aboveLevel(2, _zombieId) {
-      require(msg.sender == zombieToOwner[_zombieId]);
-      zombies[_zombieId].name = _newName;
-  }
+    function withdraw() external onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+    function setLevelUpFee(uint _fee) external onlyOwner {
+        levelUpFee = _fee;
+    }
 
-  function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
-      require(msg.sender == zombieToOwner[_zombieId]);
-      zombies[_zombieId].dna = _newDna;
-  }
+function levelUp(uint _zombieId) external payable{
+    require(msg.value == levelUpFee);
+    zombies[_zombieId].level++;
+}
 
-  function getZombiesByOwner(address _owner) external view returns(uint[] memory){
+    function changeName(uint256 _zombieId, string memory _newName)
+        external
+        aboveLevel(2, _zombieId) ownerOf(_zombieId)
+    {
+        zombies[_zombieId].name = _newName;
+    }
 
+    function changeDna(uint256 _zombieId, uint256 _newDna)
+        external 
+        aboveLevel(20, _zombieId) ownerOf(_zombieId)
+    {        
+        zombies[_zombieId].dna = _newDna;
+    }
 
-  }
-
+    function getZombiesByOwner(address _owner)
+        external
+        view
+        returns (uint256[] memory)
+    {
+        uint256[] memory result = new uint256[](ownerZombieCount[_owner]);
+        uint256 counter = 0;
+        for (uint256 i = 0; i < zombies.length; i++) {
+            if (zombieToOwner[i] == _owner) {
+                result[counter] = i;
+                counter++;
+            }
+        }
+        return result;
+    }
 }
