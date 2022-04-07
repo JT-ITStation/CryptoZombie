26683c8e211c89b1fe17cbafb9aea8d5a203415d
it4station@gmail.com <it4station@gmail.com>
2022-04-07 14:20:09 +0200

    end lesson 3

---
+++ contracts/zombiefactory.sol
@@ -0,0 +1,46 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+import "./ownable.sol";
+
+contract ZombieFactory is Ownable {
+    event NewZombie(uint256 zombieId, string name, uint256 dna);
+    uint256 cooldownTime = 1 days;
+    uint256 dnaDigits = 16;
+    uint256 dnaModulus = 10 ^ dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint256 dna;
+        uint32 level;
+        uint32 readyTime;
+    }
+    Zombie[] public zombies;
+    mapping(uint256 => address) public zombieToOwner;
+    mapping(address => uint256) ownerZombieCount;
+
+    function _createZombie(string memory _name, uint256 _dna) internal {
+        
+        zombies.push(Zombie(_name, _dna,1,uint32(block.timestamp+cooldownTime)));
+        uint256 id = zombies.length - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender] ++;
+        emit NewZombie(id, _name, _dna);
+    }
+
+    function _generateRandomDna(string memory _str)
+        private
+        view
+        returns (uint256)
+    {
+        uint256 rand = uint256(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string memory _name) public {
+        require(ownerZombieCount[msg.sender]==0, "Creation d'1 seul zombie par adresse");
+        uint256 randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+}
+
+
